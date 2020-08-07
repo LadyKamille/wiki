25d5c181da711d3f8a419571c997e2faa11c18ce
Kamille Norris <kamionorato@gmail.com>
2020-08-07 18:24:55 -0400

    Entry page and not found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, entry):
+    return render(request, "entry/index.html", {
+        "title": entry,
+        "entry": util.get_entry(entry),
+    })
+
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
